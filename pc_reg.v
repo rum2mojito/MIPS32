71746b06e9d9fc7f99dc53f54afa4223eb972e1a
Stone <wead3105@sjtu.edu.cn>
2018-02-13 12:35:22 +0800

    Update pc_reg.v

--- pc_reg.v
+++ pc_reg.v
@@ -1,26 +1,27 @@
+`include "define.v"
+
 module pc_reg (
 	input wire clk, //clock signal
 	input wire rst, //reset signal
 	
-	output reg[5:0] pc, //program counter
+	output reg[`InstAddrBus] pc, //program counter
 	output reg ce // !!!???
 );
 
 	always @ (posedge clk) begin
-		if (rst == 1'b0) begin
-			ce <= 1'b0;
+		if (rst == `RstEnable) begin
+			ce <= `ChipDisable;
 		end else begin
-			ce <= 1'b1;
+			ce <= `ChipEnable;
 		end
 	end
 	
 	always @ (posedge clk) begin
-		if (ce == 1'b0) begin
-			pc <= 6'h00;
+		if (ce == `ChipDisable) begin
+			pc <= 32'h00000000;
 		end else begin
-			pc <= pc + 1'b1;
+			pc <= pc + 4'h4;
 		end
 	end
 	
-endmodule
-				
+endmodule			
