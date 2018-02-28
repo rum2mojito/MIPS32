d09a819fd1c80dd7d38a19c630e8509355b20531
rum2mojito <wead3105@sjtu.edu.cn>
2018-02-28 16:49:37 +0800

    cpu pipeline hazards solution

--- id.v
+++ id.v
@@ -21,7 +21,17 @@ module id(
 	output reg[`RegBus] reg1_o,
 	output reg[`RegBus] reg2_o,
 	output reg[`RegAddrBus] wd_o,
-	output reg wreg_o	
+	output reg wreg_o,
+	
+	// result of execusion
+	input wire ex_wreg_i,
+	input wire[`RegBus] ex_wdata_i,
+	input wire[`RegAddrBus] ex_wd_i,
+	
+	// result of mem
+	input wire mem_wreg_i,
+	input wire[`RegBus] mem_wdata_i,
+	input wire[`RegAddrBus] mem_wd_i
 );
 
 	// get function code and operation code
@@ -98,6 +108,10 @@ module id(
 	always @ (*) begin
 		if (rst == `RstEnable) begin
 			reg1_o <= `ZeroWord;
+		end else if ((reg1_read_o == 1'b1) && (ex_wreg_i == 1'b1) && (ex_wd_i == reg1_addr_o)) begin
+			reg1_o <= ex_wdata_i;
+		end else if ((reg1_read_o == 1'b1) && (mem_wreg_i == 1'b1) && (mem_wd_i == reg1_addr_o)) begin
+			reg1_o <= mem_wdata_i;
 		end else if (reg1_read_o == 1'b1) begin
 			reg1_o <= reg1_data_i;
 		end else if (reg1_read_o == 1'b0) begin
@@ -111,6 +125,10 @@ module id(
 	always @ (*) begin
 		if (rst == `RstEnable) begin
 			reg2_o <= `ZeroWord;
+		end else if ((reg2_read_o == 1'b1) && (ex_wreg_i == 1'b1) && (ex_wd_i == reg2_addr_o)) begin
+			reg2_o <= ex_wdata_i;
+		end else if ((reg2_read_o == 1'b1) && (mem_wreg_i == 1'b1) && (mem_wd_i == reg2_addr_o)) begin
+			reg2_o <= mem_wdata_i;
 		end else if (reg2_read_o == 1'b1) begin
 			reg2_o <= reg2_data_i;
 		end else if (reg2_read_o == 1'b0) begin
